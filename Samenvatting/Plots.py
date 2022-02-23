62cedadd19b5f94351ed3b50fa956e8a9747237a
debber1 <robbe.decapmaker@hotmail.be>
2022-02-23 15:45:08 +0100

    Adding derivation 3

--- Samenvatting/Plots.py
+++ Samenvatting/Plots.py
@@ -31,4 +31,19 @@ plt.yticks([])
 plt.text(-np.pi/2 +0.1, 0.1, '- \u03C6')
 plt.text(-2.8, 0.9, 'A')
 plt.text(-2.8, -0.95, '- A')
+plt.show()
 plt.savefig('Harmonische_Oscillator.pdf')
+
+plt.clf()
+xx = np.linspace(-1,1,100)
+def f(x) : return x**2
+def g(x) : return -x**2 + 1
+
+Potential = plt.plot(xx,f(xx),'r', lw = 1)
+Kinetic = plt.plot(xx,g(xx),'g', lw = 1)
+plt.xlabel("Verplaatsing (m)")
+plt.ylabel("E(x) (J)")
+#plt.hlines(1,-1,1)
+plt.legend(loc='lower left',labels=['Potentiële energie', 'Kinetische energie'])
+#plt.show()
+plt.savefig('Energie_Balans.pdf')
